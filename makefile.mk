44bdceb61456eb8f48e9cec35a63e7d049c4efa8
Kevin Rivas-Rivera <eekeriv@gmail.com>
2015-02-14 00:44:00 -0800

    Original
    No Mods to FW

---
+++ makefile.mk
@@ -0,0 +1,23 @@
+#
+# Copyright 2014, Broadcom Corporation
+# All Rights Reserved.
+#
+# This is UNPUBLISHED PROPRIETARY SOURCE CODE of Broadcom Corporation;
+# the contents of this file may not be disclosed to third parties, copied
+# or duplicated in any form, in whole or in part, without the prior
+# written permission of Broadcom Corporation.
+#
+
+########################################################################
+# Application sources.
+########################################################################
+APP_SRC = wiced_sense.c drivers/l3gd20_driver.c drivers/hts221_driver.c drivers/lis3dsh_driver.c 
+APP_SRC += drivers/lps25h_driver.c drivers/lsm303d_driver.c
+APP_SRC += ws_sec_upgrade_ota.c ws_upgrade.c rsa_pub.c
+
+INCS += $(DIR)/drivers
+########################################################################
+################ DO NOT MODIFY FILE BELOW THIS LINE ####################
+########################################################################
+APP_PATCHES_AND_LIBS += application_poll_notification.a
+APP_PATCHES_AND_LIBS += thread_and_mem_mgmt.a rtc_api.a
